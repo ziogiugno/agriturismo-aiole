19716d1fbc3b68cd7a9a1cd92f8530def943c859
Rolando Duma <ziogiugno@gmail.com>
2020-04-05 06:59:50 +0200

    hero added

--- src/pages/products.js
+++ src/pages/products.js
@@ -8,9 +8,23 @@ import { graphql, useStaticQuery } from "gatsby";
 
 const ProductsPage = () => {
   const intl = useIntl();
-  const orto = useStaticQuery(graphql`
-    query OrtoQuery {
-      file(relativePath: { eq: "orto.jpg" }) {
+  const images = useStaticQuery(graphql`
+    query {
+      banner: file(relativePath: { eq: "banner07.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      img1: file(relativePath: { eq: "orto.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      img2: file(relativePath: { eq: "prodotti.jpg" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
@@ -19,7 +33,7 @@ const ProductsPage = () => {
       }
     }
   `);
-
+  console.log(images);
   return (
     <>
       <Header siteTitle={intl.formatMessage({ id: "products_title" })} />
@@ -29,13 +43,16 @@ const ProductsPage = () => {
           title={intl.formatMessage({ id: "products_title" })}
           keywords={[`gatsby`, `application`, `react`]}
         />
+        <section className="hero">
+          <Image fluid={images.banner.childImageSharp.fluid} />
+        </section>
         <div className="columns">
           <div className="column">
             <h2 className="is-size-3">
               <FormattedMessage id="products_block1_title" />
             </h2>
+            <Image fluid={images.img1.childImageSharp.fluid} />
             <p className="section">
-              <Image fluid={orto.file.childImageSharp.fluid} />
               <FormattedMessage id="products_block1_body" />
             </p>
           </div>
@@ -43,8 +60,8 @@ const ProductsPage = () => {
             <h2 className="is-size-3">
               <FormattedMessage id="products_block2_title" />
             </h2>
+            <Image fluid={images.img2.childImageSharp.fluid} />
             <p className="section">
-              <Image fluid={orto.file.childImageSharp.fluid} />
               <FormattedMessage id="products_block2_body" />
             </p>
           </div>
